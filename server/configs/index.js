636d28ae232cd2f203faf12658bd3828c3433dbe
管复生 <975081801@qq.com>
2017-06-25 01:04:10 +0800

    测试mongoldb数据库连接以及基本构建

---
+++ server/configs/index.js
@@ -0,0 +1,11 @@
+'use strict'
+let config = {
+    mongoConfig:{
+        url:'mongodb://localhost/test',
+        opts:{
+            user:'',
+            psw:''
+        }
+    }
+}
+module.exports = config;
